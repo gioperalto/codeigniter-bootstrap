2f45a916e87c6f96e1d210453eb321f20c007d95
Gio Peralto-Pritchard <gpera008@fiu.edu>
2015-05-08 09:55:41 -0400

    customized welcome page to have a more bootstrap-oriented feel

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,12 +10,12 @@
     <title>CodeIgniter Bootstrap</title>
 
 		<!-- CSS Calls -->
-    <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo 'assets/css/bootstrap.min.css'; ?>" rel="stylesheet">
+    <link href="<?php echo 'assets/css/font-awesome.min.css'; ?>" rel="stylesheet">
 
     <!-- JS Calls (Don't want to chance certain scripts not loading) -->
-		<script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
-  	<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+		<script src="<?php echo 'assets/js/jquery.min.js'; ?>"></script>
+  	<script src="<?php echo 'assets/js/bootstrap.min.js'; ?>"></script>
   </head>
 	<body>
 	<!-- TODO: Add body content -->
