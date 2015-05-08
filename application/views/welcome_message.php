2658b1a941aa8d0cca15dab3f26e591c60c0c7f1
gioperalto <gyoperalto@gmail.com>
2015-05-08 10:16:54 -0400

    did some more page customizations

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,10 +1,10 @@
 <div class="jumbotron">
-	<h1>Welcome to CodeIgniter <span class="small">and Bootstrap</span></h1>
+	<h1>Welcome to CodeIgniter <span class="small">and Bootstrap v3.1.1</span></h1>
 		
 </div>
 <div class="container">
-	<section id="body" class="">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter, but we've incorportated Bootstrap.</p>
+	<section id="promo">
+		<h4>The page you are looking at is being generated dynamically by CodeIgniter, but we've incorportated Bootstrap.</h4>
 
 		<p>
 			If you would like to edit this page you'll find it located at:
@@ -17,6 +17,31 @@
 		</p>
 		
 		<p class="alert alert-info">If you are exploring CodeIgniter for the very first time, you should start by reading the <a class="text-danger" href="user_guide/">User Guide</a>.</p>
+	</section>
+	<section id="fa">
+		<h4>As an added benefit, we've also integrated font-awesome v4.3.0</h4>
+		<div class="row">
+			<div class="col-sm-4 text-center">
+				<div class="text-danger">
+					<i class="fa fa-3x fa-street-view"></i>
+					<h2>Icon 1</h2>
+				</div>
+			</div>
+			<div class="col-sm-4 text-center">
+				<div class="text-success">
+					<i class="fa fa-3x fa-ship"></i>
+					<h2>Icon 2</h2>
+				</div>
+			</div>
+			<div class="col-sm-4 text-center">
+				<div class="text-primary">
+					<i class="fa fa-3x fa-diamond"></i>
+					<h2>Icon 3</h2>
+				</div>
+			</div>
+		</div>
+		<p class="alert alert-info">If you are exploring font-awesome for the very first time, you should start by checking out their <a class="text-danger" href="http://fortawesome.github.io/Font-Awesome/">website</a>.</p>
+
 	</section>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
